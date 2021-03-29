91f0b28c606d9f98f1b20613e5a868b8d98bcd9a
Remon Kamal <RemonComputer@gmail.com>
2021-03-29 04:38:33 +0200

    Replaced the cuda C++ code with pytorch module

--- lib/nn/__init__.py
+++ lib/nn/__init__.py
@@ -1,3 +1,3 @@
 from .modules import *
-from .prroi_pool import *
+# from .prroi_pool import *  # Deprecated Module use torch.nn.AdaptiveAvgPool2d
 from .parallel import UserScatteredDataParallel, user_scattered_collate, async_copy_to
